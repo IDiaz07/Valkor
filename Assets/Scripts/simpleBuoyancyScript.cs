d364f81a041dd53de319266f61bcf503950d349b
angelgandarias <angel.gandarias@opendeusto.es>
2025-11-20 12:15:35 +0100

    Test Simulación de agua

---
+++ Assets/Scripts/simpleBuoyancyScript.cs
@@ -0,0 +1,42 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class simpleBuoyancyScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private float buoyancyReduction = 20f;
+    [SerializeField]
+    private float dampingReduction= 150f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        try
+        {
+            Rigidbody rb = other.gameObject.GetComponentInParent<Rigidbody>();
+            Debug.Log(rb.gameObject.name + (this.gameObject.transform.position.y - rb.transform.TransformPoint(rb.centerOfMass).y));
+            //La profundidad a la que está sumergido el objeto
+            float depth = (this.gameObject.transform.position.y - rb.transform.TransformPoint(rb.centerOfMass).y);
+            if (depth > 0)
+            {
+                rb.AddForce(this.transform.up * (other.bounds.size.magnitude / buoyancyReduction * depth), ForceMode.Force);
+                //Amortiguación para reducir el efecto rebote
+                float damping = -rb.linearVelocity.y *1/dampingReduction;
+                rb.AddForce(transform.up * damping, ForceMode.Force);
+            }
+        }
+        catch (System.Exception e)
+        {
+        }
+    }
+}
