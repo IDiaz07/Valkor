9b63770755f9430de31478889705cdb8f235485e
angelgandarias <angel.gandarias@opendeusto.es>
2025-11-23 18:34:27 +0100

    Mejoras al comportamiento del agua
    -Ahora las objetos que flotan tienden a ponerse rectos en el agua. Además, el peso del jugador los afectará si se sube encima (WIP)

---
+++ Assets/Scripts/characterWeightSimulator.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class characterWeightSimulator : MonoBehaviour
+{
+    [SerializeField]
+    private float characterWeight;
+    [SerializeField]
+    private CharacterController characterController;
+    private Collider characterCollider;
+
+    void Awake()
+    {
+      characterController = GetComponent<CharacterController>();
+        characterCollider = GetComponent<Collider>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision);
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.AddForceAtPosition(Vector3.down * characterWeight, collision.collider.ClosestPoint(characterController.transform.position));
+        }
+        else { return;}
+    }*/
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("collision");
+        Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.AddForceAtPosition(Vector3.down * characterWeight, other.ClosestPoint(characterController.transform.position));
+            Debug.Log("forceAdded");
+        }
+        else { Debug.Log("Could not get rigidBody for " + other.gameObject.name); return; }
+    }
+}
--- Assets/Scripts/simpleBuoyancyScript.cs
+++ Assets/Scripts/simpleBuoyancyScript.cs
@@ -5,9 +5,9 @@ public class simpleBuoyancyScript : MonoBehaviour
 {
 
     [SerializeField]
-    private float buoyancyReduction = 20f;
+    private float buoyancyReduction = 0.01f;
     [SerializeField]
-    private float dampingReduction= 150f;
+    private float dampingReduction= 0.2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -24,12 +24,18 @@ public class simpleBuoyancyScript : MonoBehaviour
         try
         {
             Rigidbody rb = other.gameObject.GetComponentInParent<Rigidbody>();
-            Debug.Log(rb.gameObject.name + (this.gameObject.transform.position.y - rb.transform.TransformPoint(rb.centerOfMass).y));
             //La profundidad a la que está sumergido el objeto
             float depth = (this.gameObject.transform.position.y - rb.transform.TransformPoint(rb.centerOfMass).y);
             if (depth > 0)
             {
                 rb.AddForce(this.transform.up * (other.bounds.size.magnitude / buoyancyReduction * depth), ForceMode.Force);
+
+                Vector3 tilt = Vector3.Cross(rb.transform.up, Vector3.up);
+                rb.AddTorque(tilt * 10f, ForceMode.Force);
+
+                rb.AddTorque(-rb.angularVelocity * 2f, ForceMode.Force);
+
+
                 //Amortiguación para reducir el efecto rebote
                 float damping = -rb.linearVelocity.y *1/dampingReduction;
                 rb.AddForce(transform.up * damping, ForceMode.Force);
