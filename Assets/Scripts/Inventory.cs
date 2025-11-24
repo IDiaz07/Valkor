4fd94ab202377ec582c62a0c776d2e22543f8bdb
AnderUrien <ander.urien@opendeusto.es>
2025-11-24 11:37:15 +0100

    Inventario

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Inventory : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    public InputActionProperty thumbstickA;
+    CharacterController controller;
+
+    private int allSlots;
+    private bool inventoryEnabled;
+    public GameObject inventory;
+    private GameObject[] slot;
+    public GameObject slotHolder;
+    // Update is called once per frame
     void Start()
     {
-        
+        allSlots = slotHolder.transform.childCount;
+
+        slot = new GameObject[allSlots];
+
+        for (int i = 0; i < allSlots; i++) {
+
+
+            slot[i]= slotHolder.transform.GetChild(i).gameObject;
+        }
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if ( thumbstickA.action.WasPressedThisFrame())
+        {
+            inventoryEnabled = !inventoryEnabled;
+
+        }
+        if (inventoryEnabled)
+        {
+            inventory.SetActive(true);
+        }
+        else
+        {
+            inventory.SetActive(false); 
+        }
     }
+
+
+
 }
