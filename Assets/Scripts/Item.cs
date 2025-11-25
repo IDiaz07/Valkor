7e291ef74f2f6b78774a568f015aab1f46dddfea
AnderUrien <ander.urien@opendeusto.es>
2025-11-25 11:31:57 +0100

    clase item y slot

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string description;
+    public int itemID;
+    public string type;
+    public Sprite icon;
+    public bool pickedUp;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
