6f4b5d922e35a89902c6f97926030192189ebbfa
lucasEcheverria <lucas.echeverria@opendeusto.es>
2025-12-01 10:26:36 +0100

    Rana peleona

---
+++ Assets/@LucasAssets/Prefabs/CharacterLife.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class CharacterLife : MonoBehaviour
+{
+    [SerializeField] private float maxHealth = 100f;
+    [SerializeField] private float actualHealth;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        actualHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (actualHealth <= 0) Die();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Frog"))
+        {
+            actualHealth -= 9f;
+            Debug.Log($"Daño de rana recivido. Vida actual ={actualHealth}");
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("MORISTE DEJA DE JUGAR");
+    }
+}
