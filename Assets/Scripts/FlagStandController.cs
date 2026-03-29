18c63d3caacbe75b457db55a6a7f8d018f7eb5cf
Ángel Gandarias <angel.gandarias@opendeusto.es>
2026-03-29 13:44:47 +0200

    Detección bandera rival capturada

--- Assets/Scripts/FlagStandController.cs
+++ Assets/Scripts/FlagStandController.cs
@@ -5,6 +5,9 @@ using UnityEngine.XR.Interaction.Toolkit.Interactors;
 public class FlagStandController : MonoBehaviour
 {
     private XRSocketInteractor xrSocket;
+
+    //Si no es un socket del P1, será del P2
+    [SerializeField] bool isP1FlagStand;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
@@ -15,13 +18,27 @@ public class FlagStandController : MonoBehaviour
     private void DebugLogFlagDetected(SelectEnterEventArgs arg0)
     {
         //TODO El comportamiento que decidamos que ocurra cuando se detecta una bandera
-        Debug.Log("A Flag ("+arg0.interactableObject.transform.gameObject.name+") has been detected!");
+        Debug.Log("A Flag (" + arg0.interactableObject.transform.gameObject.name + ") has been detected!");
+        if (isP1FlagStand)
+        {
+            if (arg0.interactableObject.transform.CompareTag("P2Flag"))
+            {
+                Debug.Log("El juego debería acabar. TODO: Pensar en cómo hacer que acabe.");
+            }
+        }
+        else
+        {
+           if (arg0.interactableObject.transform.CompareTag("P1Flag"))
+            {
+                Debug.Log("El juego debería acabar. TODO: Pensar en cómo hacer que acabe.");
+            } 
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
 }
