baa0a0eea845bf34bd8cb7195eaeb6653538cc3f
ManuPrudencio <manu.prudencio@opendeusto.es>
2026-04-06 01:10:32 +0200

    Segundo Perosnaje Entero

--- Assets/VR Body/AnimateOnInput.cs
+++ Assets/VR Body/AnimateOnInput.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Unity.Netcode;
 
 [System.Serializable]
 public class AnimationInput
@@ -10,18 +11,33 @@ public class AnimationInput
     public InputActionProperty action;
 }
 
-public class AnimateOnInput : MonoBehaviour
+public class AnimateOnInput : NetworkBehaviour
 {
     public List<AnimationInput> animationInputs;
     public Animator animator;
 
-    // Update is called once per frame
     void Update()
     {
+        if (!IsOwner) return; // solo el dueño lee el input
+
         foreach (var item in animationInputs)
         {
             float actionValue = item.action.action.ReadValue<float>();
             animator.SetFloat(item.animationPropertyName, actionValue);
+            UpdateAnimationServerRpc(item.animationPropertyName, actionValue);
         }
+    }
+
+    [ServerRpc]
+    private void UpdateAnimationServerRpc(string propertyName, float value)
+    {
+        UpdateAnimationClientRpc(propertyName, value);
+    }
+
+    [ClientRpc]
+    private void UpdateAnimationClientRpc(string propertyName, float value)
+    {
+        if (IsOwner) return; // el dueño ya lo tiene aplicado
+        animator.SetFloat(propertyName, value);
     }
 }
