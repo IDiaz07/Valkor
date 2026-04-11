62f044043a994eecb0e0a392b6e76302ed4c835f
Ángel Gandarias <angel.gandarias@opendeusto.es>
2026-04-11 17:41:34 +0200

    Arreglado el horrible bug del movimiento del host

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class GameManager : NetworkBehaviour
 {
     public GameObject spawnable1;
+    public GameObject spawnable2;
     [SerializeField] private Vector3 player1SpawnPosition;
     [SerializeField] private Vector3 player2SpawnPosition;
 
@@ -23,15 +24,17 @@ public class GameManager : NetworkBehaviour
     {
         // Instantiate on the server
         Vector3 playerSpawnPosition = Vector3.zero;
+        GameObject spawnedObject;
         if (clientId == 0)
         {
             playerSpawnPosition = player1SpawnPosition;
+            spawnedObject = Instantiate(spawnable1, playerSpawnPosition, Quaternion.identity);
         }
         else
         {
             playerSpawnPosition = player2SpawnPosition;
+            spawnedObject = Instantiate(spawnable2, playerSpawnPosition, Quaternion.identity);
         }
-        GameObject spawnedObject = Instantiate(spawnable1, playerSpawnPosition , Quaternion.identity);
 
         // Spawn it across the network and assign ownership to the connected client
         spawnedObject.GetComponent<NetworkObject>().SpawnAsPlayerObject(clientId);
--- Assets/Scripts/MultiplayerPlayerSetup.cs
+++ Assets/Scripts/MultiplayerPlayerSetup.cs
@@ -1,8 +1,10 @@
 using Unity.Netcode;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.XR;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 using UnityEngine.XR.Interaction.Toolkit.Locomotion;
 
@@ -15,55 +17,98 @@ public class MultiplayerPlayerSetup : NetworkBehaviour
     {
         if (IsOwner)
         {
-            // The player you control
+            // --- THIS IS YOU (THE HOST/LOCAL PLAYER) ---
+
+            // 1. Claim the Main Camera tag locally
+            if (playerCamera != null) playerCamera.gameObject.tag = "MainCamera";
+
+            // 2. Enable local inputs safely
+            InputActionManager inputManager = GetComponentInChildren<InputActionManager>(true);
+            if (inputManager != null) inputManager.enabled = true;
+
+            // 3. Enable Camera and Audio
             playerCamera.enabled = true;
             playerListener.enabled = true;
-
-            // If you have a main menu camera, disable it now
             if (Camera.main != null && Camera.main != playerCamera)
             {
                 Camera.main.gameObject.SetActive(false);
             }
+
+            // 4. WAKE UP the Locomotion Providers 
+            var locomotionProviders = GetComponentsInChildren<LocomotionProvider>(true);
+            foreach (var provider in locomotionProviders) provider.enabled = true;
+
+            // 5. WAKE UP your Custom Movement Script
+            var jumpScript = GetComponentInChildren<PlayerMovementManager>(true);
+            if (jumpScript != null) jumpScript.enabled = true;
+
+            XRInputModalityManager xRInputModalityManager = GetComponentInChildren<XRInputModalityManager>(true);
+            if (xRInputModalityManager != null) xRInputModalityManager.enabled = true;
+
+            // 6. WAKE UP the Body Transformer
+            var bodyTransformer = GetComponentInChildren<XRBodyTransformer>(true);
+            if (bodyTransformer != null) bodyTransformer.enabled = true;
         }
         else
         {
-            // This is ANOTHER player..
+            // --- THIS IS A REMOTE PLAYER (CLIENT) ---
+
             playerCamera.enabled = false;
             playerListener.enabled = false;
-                // 1. Disable Tracking
-                TrackedPoseDriver[] poseDrivers = GetComponentsInChildren<TrackedPoseDriver>();
-                foreach (var driver in poseDrivers)
-                {
-                    driver.enabled = false;
-                }
 
-                // 2. Disable Controller Logic
-                XRInteractionGroup[] xrControllers = GetComponentsInChildren<XRInteractionGroup>();
-                foreach (var controller in xrControllers)
-                {
-                    controller.enabled = false;
-                }
+            // Untag the remote camera so it doesn't break physics
+            if (playerCamera != null) playerCamera.gameObject.tag = "Untagged";
 
-                // 3. Disable Interactors (Rays, Direct grabs, etc.)
-                XRBaseInteractor[] interactors = GetComponentsInChildren<XRBaseInteractor>();
-                foreach (var interactor in interactors)
-                {
-                    interactor.enabled = false;
-                }
+            XROrigin xrOrigin = GetComponent<XROrigin>();
+            if (xrOrigin != null) xrOrigin.enabled = false;
 
-                // 4. Disable the locomotion provider
-                var locomotionProviders = GetComponentsInChildren<LocomotionProvider>();
-                foreach (var provider in locomotionProviders)
-                {
-                    provider.enabled = false;
-                }
+            CharacterController charController = GetComponent<CharacterController>();
+            if (charController != null) charController.enabled = false;
 
-                // 5. Disable hand animation scripts
-                HandAnimator[] handAnimators = GetComponentsInChildren<HandAnimator>();
-                foreach(HandAnimator handAnimator in handAnimators)
+            // NEW: Kill the Character Controller Driver
+            CharacterControllerDriver ccDriver = GetComponentInChildren<CharacterControllerDriver>();
+            if (ccDriver != null) ccDriver.enabled = false;
+
+            LocomotionMediator locoMediator = GetComponentInChildren<LocomotionMediator>();
+            if (locoMediator != null) locoMediator.enabled = false;
+
+            // FIX: Explicitly KILL the built-in locomotion providers
+            var locomotionProviders = GetComponentsInChildren<LocomotionProvider>();
+            foreach (var provider in locomotionProviders) provider.enabled = false;
+
+            // FIX: Explicitly KILL the custom movement script your teammate added
+            var jumpScript = GetComponentInChildren<PlayerMovementManager>();
+            if (jumpScript != null) jumpScript.enabled = false;
+
+            Transform localManagers = transform.Find("LocalManagers");
+            if (localManagers != null)
+            {
+                InputActionManager inputManager = localManagers.GetComponentInChildren<InputActionManager>();
+                if (inputManager != null && inputManager.actionAssets != null)
                 {
-                    handAnimator.enabled = false;
+                    inputManager.actionAssets.Clear();
                 }
+                Destroy(localManagers.gameObject);
+            }
+
+            TrackedPoseDriver[] poseDrivers = GetComponentsInChildren<TrackedPoseDriver>();
+            foreach (var driver in poseDrivers) driver.enabled = false;
+
+            XRInteractionGroup[] xrGroups = GetComponentsInChildren<XRInteractionGroup>();
+            foreach (var group in xrGroups) group.enabled = false;
+
+            XRBaseInteractor[] interactors = GetComponentsInChildren<XRBaseInteractor>();
+            foreach (var interactor in interactors) interactor.enabled = false;
+
+            HandAnimator[] handAnimators = GetComponentsInChildren<HandAnimator>();
+            foreach (HandAnimator handAnimator in handAnimators) handAnimator.enabled = false;
+
+            XRInputModalityManager xRInputModalityManager = GetComponentInChildren<XRInputModalityManager>(true);
+            if (xRInputModalityManager != null) xRInputModalityManager.enabled = false;
+
+            // KILL the XRBodyTransformer so it doesn't hijack your locomotion
+            var bodyTransformer = GetComponentInChildren<XRBodyTransformer>();
+            if (bodyTransformer != null) bodyTransformer.enabled = false;
         }
     }
 }
