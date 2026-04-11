84e20c6dd05fe3f1ff319d74aaeac5884e8b0bb0
Ángel Gandarias <angel.gandarias@opendeusto.es>
2026-04-11 17:49:30 +0200

    Tp si un jugador se sale del mapa

--- Assets/OutOfBoundsTeleport.cs
+++ Assets/OutOfBoundsTeleport.cs
@@ -1,16 +1,35 @@
+using Unity.Netcode;
 using UnityEngine;
 
 public class OutOfBoundsTeleport : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Teleport Settings")]
+    [Tooltip("Las coordenadas del teletransporte")]
+    public Vector3 respawnPosition = new Vector3(0, 2f, 0);
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            NetworkObject netObj = other.GetComponentInParent<NetworkObject>();
+
+            //Solo el jugador dueño del player es capaz de teletransportarlo
+            if (netObj != null && netObj.IsOwner)
+            {
+                CharacterController charController = netObj.GetComponent<CharacterController>();
+
+                if (charController != null)
+                {
+                    // Se desactiva y se activa para evitar que interfiera con el teletransporte
+                    charController.enabled = false;
+                    netObj.transform.position = respawnPosition;
+                    charController.enabled = true;
+                }
+                else
+                {
+                    netObj.transform.position = respawnPosition;
+                }
+            }
+        }
     }
 }
